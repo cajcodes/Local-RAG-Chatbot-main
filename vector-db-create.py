410499ce9e6b37eecb53115befc886be722e1459
cajcodes <129537800+cajcodes@users.noreply.github.com>
2024-04-30 12:04:40 -0500

    Added annotations and local embed quickstart

--- vector-db-create.py
+++ vector-db-create.py
@@ -9,17 +9,24 @@ from langchain_community.embeddings import HuggingFaceEmbeddings
 # Initialize HuggingFaceEmbeddings
 embedding_function = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
 
+# Initialize PyPDFLoader with the path to the PDF file
 loaders = [PyPDFLoader('.uhc/Medical Member Handbook starting 1_1_2024.pdf')]
 
+# Load the PDF file
 docs = []
 for file in loaders:
     docs.extend(file.load())
 
-# Split text to chunks
+# Split the text into chunks
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
 docs = text_splitter.split_documents(docs)
 
-# Use HuggingFaceEmbeddings for generating embeddings
+# Use HuggingFaceEmbeddings to generate embeddings for the text chunks
+# The embeddings are used to create a vectorstore
+# The vectorstore is a database that stores the embeddings for each text chunk
+# The persist_directory argument specifies where the vectorstore should be saved
 vectorstore = Chroma.from_documents(docs, embedding_function, persist_directory="./chroma_db_nccn")
 
+# Print the number of documents in the vectorstore
+# Each document corresponds to a text chunk
 print(vectorstore._collection.count())
